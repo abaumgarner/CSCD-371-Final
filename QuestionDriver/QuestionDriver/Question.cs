f9626f8e9c5ad5d0e3c4f0022a86208fc1b2777f
abaumgarner <aaron.baumgarner@eagles.ewu.edu>
2015-03-05 20:44:17 -0800

    QuestionDriver
    Worked on a way to get questions and answers from the Database

---
+++ QuestionDriver/QuestionDriver/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using DatabaseHandler;
+
+namespace QuestionDriver
+{
+    public class Question
+    {
+        public Question()
+        {
+            Database.CreateDatabase();
+        }
+    }
+}
